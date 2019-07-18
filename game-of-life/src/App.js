fa20d9ee54643dbf62feec9e7a5e2411190f4c5d
Elisha Atulomah <elishaubax@gmail.com>
2019-07-18 13:46:20 -0700

    Created grid component

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Grid from './components/Grid.js';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      generation: 0,
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1 className="App-header">Game of Life</h1>
+        <Grid />
+        <h2>Generations: {this.state.generation}</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ game-of-life/src/components/Grid.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Grid extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Grid</h1>
+      </div>
+    );
+  }
+}
+
+export default Grid;
