f614d5dcd82332041058a254a1adaf029e9ae217
Elisha Atulomah <elishaubax@gmail.com>
2019-07-18 19:19:58 -0700

    Created Button and Grid components

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -6,8 +6,15 @@ import Grid from './components/Grid.js';
 class App extends React.Component {
   constructor() {
     super();
+    this.speed = 100;
+    this.rows = 30;
+    this.cols = 50;
+
     this.state = {
       generation: 0,
+      gridFull: Array(this.rows)
+        .fill()
+        .map(() => Array(this.cols).fill(false)),
     };
   }
 
@@ -15,7 +22,12 @@ class App extends React.Component {
     return (
       <div className="App">
         <h1 className="App-header">Game of Life</h1>
-        <Grid />
+        <Grid
+          gridFull={this.state.gridFull}
+          rows={this.rows}
+          cols={this.cols}
+          selectBox={this.selectBox}
+        />
         <h2>Generations: {this.state.generation}</h2>
       </div>
     );
--- game-of-life/src/components/Grid.js
+++ game-of-life/src/components/Grid.js
@@ -7,9 +7,24 @@ class Grid extends React.Component {
   }
 
   render() {
+    const width = this.props.cols * 14;
+    let rowsArr = [];
+
+    let boxClass = '';
+    for (let i = 0; i < this.props.rows; i++) {
+      for (let j = 0; j < this.props.cols; j++) {
+        const boxId = i + '_' + j;
+
+        boxClass = this.props.gridFull[i][j] ? 'box alive' : 'box dead';
+        rowsArr.push(
+          <Box boxClass={boxClass} key={boxId} boxId={boxId} row={i} col={j} selectBox={this.props.selectBox} />,
+        );
+      }
+    }
+
     return (
-      <div>
-        <h1>Grid</h1>
+      <div className="grid" style={{ width: width }}>
+        {rowsArr}
       </div>
     );
   }
