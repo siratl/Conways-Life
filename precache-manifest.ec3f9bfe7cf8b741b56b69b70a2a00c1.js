b4973a50c8fadf1c7e5603821f37e6c23aaffbd7
Elisha Atulomah <elishaubax@gmail.com>
2019-07-19 11:23:04 -0700

    Updates

---
+++ precache-manifest.ec3f9bfe7cf8b741b56b69b70a2a00c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baa15b6b9d7f0eff52c14690c227ab63",
+    "url": "/Conways-Life/index.html"
+  },
+  {
+    "revision": "d3cf6c2b08bdddd282ed",
+    "url": "/Conways-Life/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "81671b7106cbfd120839",
+    "url": "/Conways-Life/static/css/main.8d059753.chunk.css"
+  },
+  {
+    "revision": "d3cf6c2b08bdddd282ed",
+    "url": "/Conways-Life/static/js/2.80d09100.chunk.js"
+  },
+  {
+    "revision": "81671b7106cbfd120839",
+    "url": "/Conways-Life/static/js/main.13ff2793.chunk.js"
+  },
+  {
+    "revision": "f94638f27adfa8c548ad",
+    "url": "/Conways-Life/static/js/runtime~main.5602915e.js"
+  }
+]);
